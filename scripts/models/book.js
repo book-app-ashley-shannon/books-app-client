a866161091eeeb388ab6270ff0b56c99e50cb861
Shannon Tully <scatman.st@gmail.com>
2017-12-19 12:13:14 -0800

    load/fetch

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,8 +3,6 @@
 var __API_URL__ = 'http//localhost:3000';
 var __API_URL__ = 'https:st-aj-booklist.herokuapp.com';
 
-var books = []
-
 (function() {
   function Book(title, author, isbn, image_url, description) {
     this.title = title;
@@ -12,11 +10,25 @@ var books = []
     this.isbn = isbn;
     this.image_url = image_url;
     this.description = description;
-    books.push(this);
   };
 
-  Book.prototype.toHtml() {
+  Book.prototype.toHtml = function() {
     var template = Handlebars.compile($('#book-list-template').html());
     books.forEach(ele => $('section').append(template(ele)));
+  };
+
+  Book.all = [];
+
+  Book.loadAll = rows => {
+    rows.sort((a, b) => b.title - a.title);
+    Book.all = rows.map(ele => new Book(ele));
+  };
+
+  Book.fetchAll = callback => {
+    $.get('/api/v1/books')
+    .then(results => {
+      Book.loadAll(callback);
+      callback();
+    })
   };
 })();
