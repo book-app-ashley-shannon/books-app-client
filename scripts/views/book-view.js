b934668d38e2161c94ac4615c883050cb2528176
Ashley Jordan <ajordanium@gmail.com>
2017-12-20 12:13:10 -0800

    tried 500 things to get the stupid details button to work and still nothing

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -9,6 +9,20 @@ var app = app || {};
       $('.container').hide();
       $('.book-view').show();
       module.Book.all.map(ele => $('#book-list').append(ele.toHtml()));
+      let bookid;
+      $('.detail-view').on('click', 'button.db', app.bookView.getBookid);
+    };
+
+    bookView.getBookid = () => {
+      console.log(this);
+      app.Book.fetchOne(app.bookView.initDetailPage);
+    };
+
+    bookView.initDetailPage = err => {
+      $('.container').hide();
+      $('.detail-view').show();
+      let template = Handlebars.compile($('#detail-template').text());
+      $('.detail-view').append(template(err));
     };
     module.bookView = bookView;
 })(app);
