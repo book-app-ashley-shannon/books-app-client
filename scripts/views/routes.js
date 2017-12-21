a63a433ad95eabc61b6ced17f958357175eee0c6
Shannon Tully <scatman.st@gmail.com>
2017-12-20 16:08:03 -0800

    removed event listener

---
+++ scripts/views/routes.js
@@ -0,0 +1,6 @@
+'use strict';
+
+page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
+page('/books/:id', ctx => app.Book.fetchOne(app.bookView.initDetailPage, ctx.params.id));
+
+page();
