aaa17ca5155decb3424c8aa55aa4d729244dc901
Ashley Jordan <ajordanium@gmail.com>
2017-12-21 10:26:21 -0800

    added form to index page, other things

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -2,6 +2,6 @@
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/:id', ctx => app.Book.fetchOne(app.bookView.initDetailPage, ctx.params.id));
-// page('/books/new', app.bookView.initFormPage());
+page('/books/new', app.bookView.initFormPage);
 
 page();
